dd353318579c1298b641d7da068f5f37d8238509
Nandivaleamol <nandivaleamol@gmail.com>
2022-11-09 09:54:49 +0530

    Spring boot validation project

---
+++ src/main/java/com/spring/boot/validation/entity/User.java
@@ -0,0 +1,15 @@
+package com.spring.boot.validation.entity;
+
+public class User {
+
+    @Id
+    private int userId;
+    private String name;
+    private String email;
+    private String mobile;
+    private String gender;
+    private int age;
+    private String nationality;
+
+
+}
